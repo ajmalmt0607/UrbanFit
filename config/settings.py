842097ac0a9a037e35d198b115fc2e69ac57b60e
Mohammed Ajmal M T <ajmalmt@Mohammeds-MacBook-Air.local>
2026-05-17 12:15:52 +0530

    Configure Gmail SMTP for OTP emails

--- config/settings.py
+++ config/settings.py
@@ -164,6 +164,24 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+# Email settings
+EMAIL_BACKEND = env(
+    "EMAIL_BACKEND",
+    default="django.core.mail.backends.smtp.EmailBackend",
+)
+
+EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = env.int("EMAIL_PORT", default=587)
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
+
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER)
+
+DEFAULT_BCC_EMAIL = env.list("DEFAULT_BCC_EMAIL", default=[])
+DEFAULT_REPLY_TO_EMAIL = env.list("DEFAULT_REPLY_TO_EMAIL", default=[])
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
